61e7476bfabae3a2772ee8b815cda8c2dd7158f0
Swastik Kumar <swastiktovisit1234@gmail.com>
2022-12-20 10:28:21 +0530

    minute_updates_in_structure_of_directories

--- Assignment-1/Ass_1-Q-6.java
+++ Assignment-1/Ass_1-Q-6.java
@@ -4,7 +4,7 @@ class Main
 	    String s = "HelloWorld";
 	    int l=3, r=7;
 	    
-	    for(int i=3 ; i<r ; i++){
+	    for(int i=l ; i<r ; i++){
 	        System.out.print(s.charAt(i));
 	    }
 	}
